242e0fd4d47924ef7c2238a1812871c940a89e71
Nathan Sharpe <nathanjsharpe@gmail.com>
2016-12-14 17:23:33 -0500

    Add image to db seeds

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -9,23 +9,23 @@
 Gloat.destroy_all
 User.destroy_all
 
-randomUserUri = URI('https://randomuser.me/api/')
+randomUserUri = URI('https://randomuser.me/api/?results=10')
 
-10.times do |i|
-  response = Net::HTTP.get(randomUserUri)
-  user = JSON.parse(response, symbolize_names: true)[:results].first
-  User.create({
-    email: user[:email],
-    password: 'password',
-    password_confirmation: 'password',
-    city: user[:location][:city],
-    state: user[:location][:state],
-    profession: Faker::Company.profession,
-    company: Faker::Company.name,
-    name: [user[:name][:first], user[:name][:last]].join(' ').titleize,
-    username: user[:login][:username]
-  })
-end
+response = Net::HTTP.get(randomUserUri)
+users = JSON.parse(response, symbolize_names: true)[:results]
+User.create(users.map{|user| {
+  email: user[:email],
+  password: 'password',
+  password_confirmation: 'password',
+  city: user[:location][:city],
+  state: user[:location][:state],
+  profession: Faker::Company.profession,
+  company: Faker::Company.name,
+  name: [user[:name][:first], user[:name][:last]].join(' ').titleize,
+  username: user[:login][:username],
+  image: user[:picture][:medium]
+
+}})
 
 users = User.all
 
