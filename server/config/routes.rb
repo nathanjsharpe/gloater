42a68858c730302aed603824da9a080cbee6654d
Nathan Sharpe <nathan@cliftonlabs.com>
2016-12-15 12:07:24 -0500

    Add routes for stalks

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   resources :api_tokens
-  resources :users, except: [:update, :destroy]
+  resources :users, except: [:update, :destroy] do
+    resource :stalk, only: [:create, :destroy]
+  end
   resources :admires, path: 'admired_gloats', only: [:index]
+  resources :stalks, path: 'stalked_users', only: [:index]
   resources :gloats do
     resource :admire, only: [:create, :destroy]
   end
