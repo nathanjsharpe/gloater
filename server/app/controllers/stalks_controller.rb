0e627014a68f5202f20e4e8bf0d6e6e45b580e09
Nathan Sharpe <nathan@cliftonlabs.com>
2016-12-15 12:20:00 -0500

    Add stalks controller

--- server/app/controllers/stalks_controller.rb
+++ server/app/controllers/stalks_controller.rb
@@ -1,2 +1,25 @@
 class StalksController < ApplicationController
+  before_action :authenticate!
+  before_action :set_user, only: [:create, :destroy]
+
+  def index
+    render json: current_user.stalked_users
+  end
+
+  def create
+    current_user.stalked_users << @user
+    current_user.save
+    @user.reload
+    render json: @user
+  end
+
+  def destroy
+    current_user.stalked_users.delete(@user)
+    current_user.save
+  end
+
+  private
+    def set_user
+      @user = User.find(params[:user_id])
+    end
 end
